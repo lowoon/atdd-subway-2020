b86d981e078c4efb3a4ce524ff751e20e24bdae7
lowoon <sangbo0229@gmail.com>
2020-08-06 16:03:43 +0900

    docs: 최단 시간 조회 documentation

--- src/test/java/wooteco/subway/maps/map/documentation/PathDocumentation.java
+++ src/test/java/wooteco/subway/maps/map/documentation/PathDocumentation.java
@@ -68,4 +68,35 @@ public class PathDocumentation extends Documentation {
                     fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리")))).
             extract();
     }
+
+    @Test
+    void findDurationPath() {
+        List<StationResponse> stations = Arrays.asList(
+            new StationResponse(1L, "광교중앙역", LocalDateTime.now(), LocalDateTime.now()),
+            new StationResponse(2L, "잠실역", LocalDateTime.now(), LocalDateTime.now()));
+        PathResponse pathResponse = new PathResponse(stations, 5, 10);
+        when(mapService.findPath(any(), any(), any())).thenReturn(pathResponse);
+
+        given().log().all().
+            contentType(MediaType.APPLICATION_JSON_VALUE).
+            param("source", 1L).
+            param("target", 2L).
+            param("type", "DURATION").
+            when().
+            get("/paths").
+            then().
+            log().all().
+            apply(document("paths/duration",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                responseFields(
+                    fieldWithPath("stations").type(JsonFieldType.ARRAY).description("역 목록"),
+                    fieldWithPath("stations[0].id").type(JsonFieldType.NUMBER).description("시작역 id"),
+                    fieldWithPath("stations[0].name").type(JsonFieldType.STRING).description("시작역 이름"),
+                    fieldWithPath("stations[1].id").type(JsonFieldType.NUMBER).description("도착역 id"),
+                    fieldWithPath("stations[1].name").type(JsonFieldType.STRING).description("도착역 이름"),
+                    fieldWithPath("duration").type(JsonFieldType.NUMBER).description("시간"),
+                    fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리")))).
+            extract();
+    }
 }
